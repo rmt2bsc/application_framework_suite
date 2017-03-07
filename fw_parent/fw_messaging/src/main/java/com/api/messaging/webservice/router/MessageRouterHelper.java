0712895d6c5f89843e8d115283bd82ea25b22238
appdev <royterrell2@gmail.com>
2017-03-07 15:25:43 -0600

    Modfiied source to where we can route SOAP, JSON, and XML messages to the API Business Server and output the request and response payload content to the logs.

--- fw_parent/fw_messaging/src/main/java/com/api/messaging/webservice/router/MessageRouterHelper.java
+++ fw_parent/fw_messaging/src/main/java/com/api/messaging/webservice/router/MessageRouterHelper.java
@@ -15,17 +15,19 @@ import com.api.messaging.handler.MessageHandlerResults;
 import com.api.messaging.webservice.soap.SoapMessageHelper;
 import com.api.messaging.webservice.soap.client.SoapBuilderException;
 import com.api.xml.jaxb.JaxbUtil;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
- * Helper class for routing various types of messages from one API to another.
+ * Helper class for routing various types of messages to their respective
+ * destinations via either a RMI, HTTP, or JMS transport.
  * 
  * @author Roy Terrell
  * 
  */
 public class MessageRouterHelper extends RMT2Base {
 
-    private static final Logger logger = Logger
-            .getLogger(MessageRouterHelper.class);
+    private static final Logger logger = Logger.getLogger(MessageRouterHelper.class);
 
     /**
      * Create a MessageRouterHelper object
@@ -35,8 +37,7 @@ public class MessageRouterHelper extends RMT2Base {
     }
 
     /**
-     * Route a SOAP message to its appropriate destination via either a RMI,
-     * HTTP, or JMS transport.
+     * Route a SOAP message to its appropriate destination.
      * 
      * @param messageId
      *            The message id of the service that requires the SOAP message
@@ -46,16 +47,14 @@ public class MessageRouterHelper extends RMT2Base {
      * @return an instance of {@link SOAPMessage} as the reply
      * @throws MessageRoutingException
      */
-    public SOAPMessage routeSoapMessage(String messageId, SOAPMessage payload)
-            throws MessageRoutingException {
+    public SOAPMessage routeSoapMessage(String messageId, SOAPMessage payload) throws MessageRoutingException {
         MessagingRouter router = MessageRouterFactory.createSoapMessageRouter();
         try {
             SoapMessageHelper helper = new SoapMessageHelper();
             String soapStr = helper.toString(payload);
             logger.info("Routing SOAP Request: ");
             logger.info(soapStr);
-            SOAPMessage results = (SOAPMessage) router.routeMessage(messageId,
-                    payload);
+            SOAPMessage results = (SOAPMessage) router.routeMessage(messageId, payload);
             soapStr = helper.toString(results);
             logger.info("Received SOAP Response: ");
             logger.info(soapStr);
@@ -67,44 +66,65 @@ public class MessageRouterHelper extends RMT2Base {
     }
 
     /**
-     * Route a general message object to its appropriate destination via either
-     * a RMI, HTTP, or JMS transport.
+     * Route a JSON message to its appropriate destination.
      * 
      * @param messageId
      *            The message id of the service to route payload.
      * @param payload
-     *            The payload is a Serializable object
-     * @return Serializable object as the reply
+     *            The payload is a Serializable object that can be
+     *            marshalled/unmarshalled as JSON
+     * @return Serializable object as the reply which is expected to be an
+     *         object that can be marshalled/unmarshalled via JAXB.
      * @throws MessageRoutingException
      */
-    public Object routeSerialMessage(String messageId, Serializable payload)
-            throws MessageRoutingException {
-        MessagingRouter router = MessageRouterFactory.createJaxbMessageRouter();
+    public Object routeJsonMessage(String messageId, Serializable payload) throws MessageRoutingException {
+        // Try to marshal request payload as JSON and dump contents to logger
+        final Gson gson = new GsonBuilder().create();
+        String jsonReq = gson.toJson(payload);
+        logger.info("Routing Request: ");
+        logger.info(jsonReq);
+
+        // Route message to business server
+        Serializable jaxPayload = this.sendPayload(messageId, payload);
+
+        // Try to marshal response payload as JSON and dump contents to logger
+        String jsonResp = gson.toJson(jaxPayload);
+        logger.info("Routing Response: ");
+        logger.info(jsonResp);
+
+        return jaxPayload;
+    }
+
+    /**
+     * Route a XML message object to its appropriate destination.
+     * 
+     * @param messageId
+     *            The message id of the service to route payload.
+     * @param payload
+     *            The payload is a Serializable object that can be
+     *            marshalled/unmarshalled as XML
+     * @return Serializable object as the reply which is expected to be an
+     *         object that can be marshalled/unmarshalled via JAXB.
+     * @throws MessageRoutingException
+     */
+    public Object routeXmlMessage(String messageId, Serializable payload) throws MessageRoutingException {
+        JaxbUtil util = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
         String xml = null;
-        // // Try to marshal request payload as XML and dump contents to logger
-        // // in the event it is a JAXB object.
-        // try {
-        // xml = MessagingResourceFactory.getJaxbMessageBinder(payload)
-        // .marshalMessage(payload);
-        // logger.info("Routing Request: ");
-        // logger.info(xml);
-        // } catch (Exception e) {
-        // // Do nothing...not a JAXB object
-        // }
-        Serializable results = (Serializable) router.routeMessage(messageId,
-                payload);
-        Serializable jaxPayload = null;
-        if (results instanceof MessageHandlerResults) {
-            jaxPayload = ((MessageHandlerResults) results).getPayload();
-        }
-        // Try to marshal response payload as XML and dump contents to logger
-        // in the event it is a JAXB object.
+        // Try to marshal request payload as XML and dump contents to logger
         try {
-            JaxbUtil util = SystemConfigurator
-                    .getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+            xml = util.marshalMessage(payload);
+            logger.info("Routing Response: ");
+            logger.info(xml);
+        } catch (Exception e) {
+            // Do nothing...not a JAXB object
+        }
+
+        // Route message to business server
+        Serializable jaxPayload = this.sendPayload(messageId, payload);
+
+        // Try marshall response payload as XML and dump contents to logger
+        try {
             xml = util.marshalMessage(jaxPayload);
-            // xml = MessagingResourceFactory.getJaxbMessageBinder()
-            // .marshalMessage(jaxPayload);
             logger.info("Routing Response: ");
             logger.info(xml);
         } catch (Exception e) {
@@ -113,4 +133,27 @@ public class MessageRouterHelper extends RMT2Base {
         return jaxPayload;
     }
 
+    /**
+     * Sends a generic message payload to it's appropriate destination.
+     * <p>
+     * The payload is expcected to be in the form of a serializable object that
+     * can be marshalled and unmarshalled via JAXB implementation.
+     * 
+     * @param messageId
+     *            The message id of the service to route payload.
+     * @param payload
+     *            The message payload
+     * @return Serializable object as the reply
+     * @throws MessageRoutingException
+     */
+    protected Serializable sendPayload(String messageId, Serializable payload) throws MessageRoutingException {
+        MessagingRouter router = MessageRouterFactory.createJaxbMessageRouter();
+        // Route message to business server
+        Serializable results = (Serializable) router.routeMessage(messageId, payload);
+        Serializable jaxbPayload = null;
+        if (results instanceof MessageHandlerResults) {
+            jaxbPayload = ((MessageHandlerResults) results).getPayload();
+        }
+        return jaxbPayload;
+    }
 }
