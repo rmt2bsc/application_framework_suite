d79c09808a4c9271d357afa5f282e164285ebfbc
roy terrell <rmt2bsc@gmail.com>
2017-04-04 15:34:02 -0500

    Removed outdated unused methods.

--- fw_parent/fw_messaging/src/main/java/com/api/messaging/MessagingRouter.java
+++ fw_parent/fw_messaging/src/main/java/com/api/messaging/MessagingRouter.java
@@ -1,7 +1,5 @@
 package com.api.messaging;
 
-import com.InvalidDataException;
-import com.NotFoundException;
 import com.api.messaging.handler.MessageHandlerResults;
 
 /**
@@ -33,89 +31,4 @@ public interface MessagingRouter {
      */
     MessageHandlerResults routeMessage(MessageRoutingInfo srvc, Object message)
             throws MessageRoutingException;
-
-    /**
-     * Sends a message to its intended destination.
-     * <p>
-     * This implementation must obtain the message routing inforamtion using the
-     * message id input parameter.
-     * 
-     * @param messageId
-     *            The id of the message to send.
-     * @param message
-     *            An arbitrary object representing the actual message payload
-     * @return an instance of {@link MessageHandlerResults}
-     * @throws MessageRoutingException
-     */
-    MessageHandlerResults routeMessage(String messageId, Object message)
-            throws MessageRoutingException;
-
-    /**
-     * Obtain routing information for a specified message.
-     * <p>
-     * The routing information contains the networking details about how to
-     * contact the appropriate web service handler designated to process the
-     * message.
-     * 
-     * @param messageId
-     *            the message id to use to obtain message routing information.
-     * @return an instance of {@link MessageHandlerResults}
-     * @throws MessageRoutingException
-     *             Routing information is unobtainable due to the occurrence of
-     *             data access errors or etc.
-     * @throws InvalidDataException
-     *             Routing information is unobtainable due to invalid data.
-     * @throws NotFoundException
-     *             Routing information is not found by the supplied key,
-     *             <i>messageId</i>
-     */
-    MessageRoutingInfo getRoutingInfo(String messageId)
-            throws InvalidDataException, NotFoundException,
-            MessageRoutingException;
-
-    // /**
-    // * Create the input data for a specific message handler using the incoming
-    // generic message.
-    // *
-    // * @param inMessage
-    // * an arbitrary object representing the incoming message data.
-    // * @return
-    // * an instance of {@link MessageHandlerInput}
-    // * @throws MessageRoutingException
-    // * Routing information is unobtainable due to the occurrence of data
-    // access
-    // * errors or etc.
-    // */
-    // MessageHandlerInput createReceptorInputData(Object inMessage) throws
-    // MessageRoutingException;
-    //
-    // /**
-    // * Builds the response message from the service handler's results.
-    // *
-    // * @param results
-    // * An instance of {@link MessageHandlerResults} containing the results of
-    // the service
-    // * handler processing.
-    // * @return
-    // * An arbitrary object befitting for the messaging router's
-    // implementation.
-    // * @throws InvalidDataException
-    // * @throws MessageRoutingException
-    // */
-    // Object getReceptorResults(MessageHandlerResults results) throws
-    // InvalidDataException, MessageRoutingException;
-
-    // /**
-    // * Retrieves the total number of services contained in the service
-    // registry.
-    // */
-    // Integer getServiceCount();
-
-    // /**
-    // * Sets the user's request object.
-    // *
-    // * @param request
-    // * an instance of {@link HttpServletRequest}
-    // */
-    // void setUserRequest(HttpServletRequest request);
 }
