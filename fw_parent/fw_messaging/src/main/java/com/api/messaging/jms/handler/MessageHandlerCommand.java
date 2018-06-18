e7e608eae420df7af2146b4ef6ae1e6b910eebfd
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-06-18 14:30:27 -0500

    IS-2: Changed the structure of this interface to be generic.

--- fw_parent/fw_messaging/src/main/java/com/api/messaging/jms/handler/MessageHandlerCommand.java
+++ fw_parent/fw_messaging/src/main/java/com/api/messaging/jms/handler/MessageHandlerCommand.java
@@ -7,8 +7,14 @@ import com.api.messaging.handler.MessageHandlerResults;
 /**
  * This interface describes what a command can do. It only defines one method,
  * execute, that performs the bulk of the job.
+ * 
+ * @author roy.terrell
+ *
+ * @param <T1> The request type to process
+ * @param <T2> The response type to process
+ * @param <P> The Payload type to process
  */
-public interface MessageHandlerCommand {
+public interface MessageHandlerCommand<T1, T2, P> {
     /**
      * Processes the messaging command.
      * 
