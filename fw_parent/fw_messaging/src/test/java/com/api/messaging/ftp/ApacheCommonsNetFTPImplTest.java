e946527b3ba4e8e6f749f216ede3bf53fe665a2c
appdev <royterrell2@gmail.com>
2020-12-23 18:40:22 -0600

    IS-50:  Added more test cases concerning validations.

--- fw_parent/fw_messaging/src/test/java/com/api/messaging/ftp/ApacheCommonsNetFTPImplTest.java
+++ fw_parent/fw_messaging/src/test/java/com/api/messaging/ftp/ApacheCommonsNetFTPImplTest.java
@@ -19,6 +19,10 @@ import com.api.util.RMT2Money;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ ApacheCommonsNetFTPImpl.class, FtpFactory.class, RMT2Money.class })
 public class ApacheCommonsNetFTPImplTest {
+    private static final String TEST_HOST = "192.168.0.5";
+    private static final String TEST_PORT = "21";
+    private static final String TEST_USER = "royterrell";
+    private static final String TEST_PASSWORD = "hoover";
 
     /**
      * @throws java.lang.Exception
@@ -38,9 +42,55 @@ public class ApacheCommonsNetFTPImplTest {
 
     @Test
     public void testSuccess_Connection() {
-        FtpApi api = FtpFactory.getInstance("192.168.0.5", "21", "royterrell", "hoover");
+        FtpApi api = FtpFactory.getInstance(TEST_HOST, TEST_PORT, TEST_USER, TEST_PASSWORD);
         Assert.assertNotNull(api);
     }
 
+    @Test
+    public void testSuccess_Connection_With_Default_Port() {
+        FtpApi api = FtpFactory.getInstance(TEST_HOST, null, TEST_USER, TEST_PASSWORD);
+        Assert.assertNotNull(api);
+    }
 
+    @Test
+    public void testError_Connection_With_Invalid_Host() {
+        FtpApi api = FtpFactory.getInstance("rmtdaldev77", TEST_PORT, TEST_USER, TEST_PASSWORD);
+        Assert.assertNull(api);
+    }
+
+    @Test
+    public void testError_Connection_With_Invalid_Port() {
+        FtpApi api = FtpFactory.getInstance(TEST_HOST, "100", TEST_USER, TEST_PASSWORD);
+        Assert.assertNull(api);
+    }
+
+    @Test
+    public void testError_Connection_With_NonNumeric_Port() {
+        FtpApi api = FtpFactory.getInstance(TEST_HOST, "ABC", TEST_USER, TEST_PASSWORD);
+        Assert.assertNull(api);
+    }
+
+    @Test
+    public void testError_Connection_With_Null_User() {
+        FtpApi api = FtpFactory.getInstance(TEST_HOST, TEST_PORT, null, TEST_PASSWORD);
+        Assert.assertNull(api);
+    }
+
+    @Test
+    public void testError_Connection_With_Invalid_User() {
+        FtpApi api = FtpFactory.getInstance(TEST_HOST, TEST_PORT, "testuser", TEST_PASSWORD);
+        Assert.assertNull(api);
+    }
+
+    @Test
+    public void testError_Connection_With_Null_Password() {
+        FtpApi api = FtpFactory.getInstance(TEST_HOST, TEST_PORT, TEST_USER, null);
+        Assert.assertNull(api);
+    }
+
+    @Test
+    public void testError_Connection_With_Invalid_Password() {
+        FtpApi api = FtpFactory.getInstance(TEST_HOST, TEST_PORT, TEST_USER, "test123");
+        Assert.assertNull(api);
+    }
 }
